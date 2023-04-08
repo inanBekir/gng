805dcc1b5ae2e20dac56cd3e2ce6ddf525299f1b
bekir inan <bbekirinan@gmail.com>
2023-04-08 04:09:57 +0200

    register user try catch block added and email made unique.

--- src/main/java/com/example/gng/controller/UserController.java
+++ src/main/java/com/example/gng/controller/UserController.java
@@ -5,13 +5,21 @@ import com.example.gng.model.User;
 import com.example.gng.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @Validated
@@ -21,8 +29,16 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity <User> createUserWithRole(@Valid @RequestBody UserDto userDto) {
-        User user = userService.createUserWithRole(userDto);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    public ResponseEntity <Object> createUserWithRole(@Valid @RequestBody UserDto userDto) {
+        try {
+            User user = userService.createUserWithRole(userDto);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            String errorMessage = e.getMessage();
+            Map<String, Object> response = new HashMap<>();
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.put("error", errorMessage);
+            return ResponseEntity.internalServerError().body(response);
+        }
     }
 }
